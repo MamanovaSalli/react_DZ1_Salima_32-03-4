c18e8c9ae24ec6df8df29aef288637f8180e384c
Salima <mamanovasalima441@gmail.com>
2024-01-16 13:42:16 +0300

    dz1

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
-import React, {useState} from 'react';
+import React from 'react';
 
-const SquareColorToggle = () => {
-    const [index, setIndex] = useState('red');
-    const [index2, setIndex2] = useState('blue');
-
-    const toggleColors = () => {
-        setIndex((prevColor) => (prevColor === 'red' ? 'blue' : 'red'));
-        setIndex2((prevColor) => (prevColor === 'blue' ? 'red' : 'blue'));
-    };
-
-    return (
-        <div>
-            <div
-                style={{
-                    width: '100px',
-                    height: '100px',
-                    backgroundColor: index,
-                    display: 'inline-block',
-                    cursor: 'pointer',
-                }}
-                onClick={toggleColors}
-            ></div>
-            <div
-                style={{
-                    width: '100px',
-                    height: '100px',
-                    backgroundColor: index2,
-                    display: 'inline-block',
-                    cursor: 'pointer',
-                }}
-                onClick={toggleColors}
-            ></div>
-        </div>
-    )
+const user = {
+    name: 'Hady Limarr',
+    imageURL: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90
 }
 
-export default SquareColorToggle;
+const user2 = {
+    name: 'Marilyn Monroe',
+    imageUrl: 'https://i.imgur.com/VCGLGhVb.jpg',
+    imageSize: 200,
+    info: 'Мэ́рилин Монро́ — американская киноактриса, секс-символ 1950-х годов, певица и модель. Стала одним из наиболее культовых образов американского кинематографа и всей мировой культуры.'
+}
+
+export default function Profile() {
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img className='avatar'
+                 src={user.imageURL}
+                 alt={"photo" + user.name}
+                 style={{width: user.imageSize, height: user.imageSize}}
+            />
+            <h1>{user2.name}</h1>
+            <img className='avatar'
+                 src={user2.imageUrl}
+                 alt={"photo" + user2.name}
+                 style={{width: user2.imageSize, height: user2.imageSize, }}
+            />
+            <p>{user2.info}</p>
+        </>
+    )
+}
 
 
 
